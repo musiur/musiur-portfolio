6b05aeba4f2bddff9aaa1cc06e2fc44bca1abb2a
MusiurAlam <musiur.opu@gmail.com>
2022-05-22 22:49:05 +0600

    about page design done

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import Layout from "../Layout/Layout";
 import "../styles/components/navbar.scss";
 import "../styles/components/projectCard.scss";
 import "../styles/globals.css";
+import "../styles/pages/about.scss";
+import "../styles/pages/contact.scss";
 import "../styles/pages/home.scss";
 
 function MyApp({ Component, pageProps }) {
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,39 @@
 const About = () => {
-    return (
-        <div>About</div>
-    )
-}
+  return (
+    <div className="about__container">
+      <div>
+        <div className="ac__picture"></div>
+        <div className="ac__description">
+          <h1>Musiur Alam Opu</h1>
+          <p>Software Engineer</p>
+          <br />
+          <h3>Technical Proficiencies with</h3>
+          <p>
+            1.5+ years of experience as a frontend developer and 8 months of
+            experience as frontend lead (Javascript Developer).
+          </p>
+          <p>
+            Technologies I use JS (ES6), React, Next JS, Gatsby, Svelte, Redux,
+            Tailwind CSS, SASS/SCSS, Restful APIs and Integration, Yarn and npm,
+            Git & Github, Figma, Illustrator, HTML5, CSS3 , Bootstrap, Node JS,
+            Express JS, MongoDB, Jira etc.
+          </p>
+          <p>
+            Understanding of design fundamentals, project management and problem
+            solving.
+          </p>
+
+          <h3>Experiences and responsibilities</h3>
+          <p>Avalon Hosting Services Ltd. (24 July 2021 - Present)<br />Jr. Software Engineer</p>
+
+          <h3>Education</h3>
+          <p>BSc in Computer Science and Engineer <br />
+          North South University, Dhaka, Bangladesh <br />
+          {"1 Jan, '20 - 1 Jan, '25 (Expected)"}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
