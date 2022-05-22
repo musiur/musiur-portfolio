bbf32632a6cad83346fca3158114d6fce7b54183
MusiurAlam <musiur.opu@gmail.com>
2022-05-22 23:52:11 +0600

    contact page design done

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,46 @@
 const Contact = () => {
-    return (
-        <div>Contact</div>
-    )
-}
+  return (
+    <div className="contact__container">
+      <div>
+        <h3>Contact</h3>
+        <form className="cc__form">
+          <div className="mb-6">
+            <label htmlFor="email" className="label__style">
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="input__style"
+              placeholder="name@flowbite.com"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="fullname" className="label__style">
+              Fullname
+            </label>
+            <input
+              type="text"
+              id="fullname"
+              className="input__style"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="message" className="label__style">
+              Your Message
+            </label>
+            <textarea type="text" id="message" className="input__style h-28" required />
+          </div>
+
+          <button type="submit" className="btn__primary mx-auto">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
