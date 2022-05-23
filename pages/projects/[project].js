cfeb06074e5cb7ac3a5006d96864a3d673fd1493
MusiurAlam <musiur.opu@gmail.com>
2022-05-24 02:50:38 +0600

    project page design done

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,10 +1,126 @@
+import { faCircleDot, faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import Carousel from "../../components/Carousel";
+import ImagesFromFolder from "../../utils/ImagesFromFolder";
+
+const iconDimention = "w-4 h-4";
+const WebsiteLink = <FontAwesomeIcon icon={faLink} className={iconDimention} />;
+const CircleDot = (
+  <FontAwesomeIcon icon={faCircleDot} className={iconDimention} />
+);
+
+const Abian2018Images = ImagesFromFolder(
+  require.context("/static/projects/abian2018", false, /\.(png|jpe?g|svg)$/)
+);
+const Abian2018Features = [
+  "Several Landing pages designed UI/UX maintaining design principles",
+  "User authentication system",
+  "Dashboard with two different view for admin and normal users",
+  "Collecting Donation information, manage automatically with admin permission, storing all the important and sensitive information private",
+];
 const Project = () => {
-    const router = useRouter();
-    const slug = router.query.project;
-    return (
-        <div className="project__container">{slug}</div>
-    )
-}
+  const router = useRouter();
+  const slug = router.query.project;
+  return (
+    <div className="project__container">
+      <div className="pc__canvas">
+        <div className="pcc__container">
+          <div className="pccc__showcase">
+            <div>
+              <h1>ABIAN2018FUND</h1>
+              <a
+                href="https://abian2018.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="pcccs_link">
+                  {WebsiteLink}
+                  <span>Visit now!</span>
+                </p>
+              </a>
+            </div>
+            <div>
+              <h3>Description</h3>
+              <p>
+                This is an organizational project. ABIAN2018FUND originated from
+                school friends to help society without any political and
+                profitible agenda.
+              </p>
+            </div>
+            <Carousel props={Abian2018Images} />
+
+            <div>
+              <h3>My role</h3>
+              <p>
+                I have been working starting from requirement analysis to
+                maintaining since December 2021. In this journey I have sorted
+                out all the key fetures we need, made system design and
+                architectural decision, implement UI/UX in frontend using
+                currently most popular technologies.
+              </p>
+            </div>
+            <div>
+              <h3>Features</h3>
+              <ul>
+                {Abian2018Features.map((feature, i) => {
+                  return (
+                    <li key={i}>
+                      <span>{CircleDot}</span>
+                      <span>{feature}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+
+            <div>
+              <h3>Technologies</h3>
+              <p>React, Next.js, Node JS, SCSS, etc.</p>
+            </div>
+          </div>
+          <div className="pccc__suggestion">
+            <h2>Suggestions</h2>
+            <ul>
+              <li>
+                <h3>NSU Origin</h3>
+                <p>
+                  NSU Origin is a group of people whom working on skillup,
+                  practice...
+                </p>
+                <Link href="/projects/nsu-origin">Read more</Link>
+              </li>
+              <li>
+                <h3>NSU Origin</h3>
+                <p>
+                  NSU Origin is a group of people whom working on skillup,
+                  practice...
+                </p>
+                <Link href="/projects/nsu-origin">Read more</Link>
+              </li>
+              <li>
+                <h3>NSU Origin</h3>
+                <p>
+                  NSU Origin is a group of people whom working on skillup,
+                  practice...
+                </p>
+                <Link href="/projects/nsu-origin">Read more</Link>
+              </li>
+              <li>
+                <h3>NSU Origin</h3>
+                <p>
+                  NSU Origin is a group of people whom working on skillup,
+                  practice...
+                </p>
+                <Link href="/projects/nsu-origin">Read more</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Project;
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,10 +4,7 @@ import {
   faCrown
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
-import Carousel from "../../components/Carousel";
-import SkeletonLoader from "../../components/skeletonLoaders/SkeletonLoader";
-import ImagesFromFolder from "../../utils/ImagesFromFolder";
+import ProjectCard from "../../components/ProjectCard";
 
 const iconDimention = "w-4 h-4";
 const Crown = <FontAwesomeIcon icon={faCrown} className={iconDimention} />;
@@ -36,26 +33,46 @@ const categories = [
   },
 ];
 
-const Abian2018Images = ImagesFromFolder(
-  require.context("/static/projects/abian2018", false, /\.(png|jpe?g|svg)$/)
-);
+const WorkingProjects = [
+  {
+    title: "ABIAN2018FUND",
+    image: "/static/home/abian2018.png",
+    link: "/projects/abian2018",
+    description:
+      "This is an organizational project. ABIAN2018FUND originated from school friends to help society without any political and profitible agenda.",
+  },
+  {
+    title: "musiur",
+    image: "/static/home/musiur.png",
+    link: "/projects/musiur",
+    description:
+      "This is my personal Portfolio site where I will includes latest information about my skills, works and other contribution to the programming communities.",
+  },
+  {
+    title: "NSU Origin",
+    image: "/static/home/nsuorigin.png",
+    link: "/projects/nsu-origin",
+    description:
+      "NSU Origin is a Group of people from North South University work together to skillup, participate in national and international compitition and enrich technological knowlege within tech anthutistic communities",
+  },
+];
 
 const Projects = () => {
   return (
     <div className="projects__container">
       <div>
-        <div className="pc__side_nav">
+        <div className="psc__side_nav">
           <h2>Project Category</h2>
           <ul>
             {categories.map((category, i) => {
               const { title, icon, amount } = category;
               return (
                 <li key={i}>
-                  <div className="pcsn__option">
+                  <div className="pscsn__option">
                     <>{icon}</>
                     <p>{title}</p>
                   </div>
-                  <div className="pcsn__badge">
+                  <div className="pscsn__badge">
                     <span>{amount > 10 ? "10+" : amount}</span>
                   </div>
                 </li>
@@ -63,43 +80,12 @@ const Projects = () => {
             })}
           </ul>
         </div>
-        <div className="pc__canvas">
-          <div className="pcc__container">
-            <div className="pccc__showcase">
-              {/* <SkeletonLoader /> */}
-              <Carousel props={Abian2018Images} />
-            </div>
-            <div className="pccc__suggestion">
-              <h2>Suggestions</h2>
-              <ul>
-                <li>
-                  <h3>NSU Origin</h3>
-                  <p>
-                    NSU Origin is a group of people whom working on skillup,
-                    practice...
-                  </p>
-                  <Link href="/projects/nsu-origin">Read more</Link>
-                </li>
-                <li>
-                  {/* <h3>NSU Origin</h3>
-                  <p>
-                    NSU Origin is a group of people whom working on skillup,
-                    practice...
-                  </p>
-                  <Link href="/projects/nsu-origin">Read more</Link> */}
-                  <SkeletonLoader />
-                </li>
-                <li>
-                  {/* <h3>NSU Origin</h3>
-                  <p>
-                    NSU Origin is a group of people whom working on skillup,
-                    practice...
-                  </p>
-                  <Link href="/projects/nsu-origin">Read more</Link> */}
-                  <SkeletonLoader />
-                </li>
-              </ul>
-            </div>
+        <div className="psc__canvas">
+          <h2>Personal Projects</h2>
+          <div className="pscc_container">
+            {WorkingProjects.map((projectData, i) => {
+              return <ProjectCard props={projectData} key={i} />;
+            })}
           </div>
         </div>
       </div>
